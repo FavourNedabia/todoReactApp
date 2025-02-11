9f9311cfc5db97101d663cc5b2cf5cd9b4d20631
FavourNedabia <senaadjoe@gmail.com>
2025-02-11 08:48:06 +0000

    projet

--- src/main/java/com/example/backend/repository/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-}
